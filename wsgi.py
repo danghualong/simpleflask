35b0c7473e374492ab2724b037fc5a354bb5a777
danghualong <dhl_345@qq.com>
2020-05-18 19:59:39 +0800

    增加是否强制升级

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
-from flask import Flask,make_response,request
+from flask import Flask,make_response,request,jsonify
 
 app=Flask(__name__)
 
-@app.route('/token',methods=['GET','POST'])
+@app.route('/version',methods=['GET'])
 def echo():
-    return make_response('What\'s the matter')
+    active_code=request.args.get('activecode')
+    result={'AppProductName':'宠物医院HIS',
+    'AppName':'PHMS','UpdateRequired':1,
+    'AppVersion':'1.0.0.0'}
+    if(active_code):
+        result['AppVersion']='1.2.6.0'
+    result['DownloadUrl']='{0}{1}_{2}.zip'.format('http://localhost:80/'
+    ,result['AppName'],result['AppVersion'].replace('.','_'))
+    return jsonify(result),200
 
 
